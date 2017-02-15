ed79c35fb1a10b72da27fd0c69841e6323ea189f
SimoneBM <Simoneboyem@gmail.com>
2017-02-15 15:46:54 +0100

    Add files via upload

---
+++ sketch02/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+
+ createCanvas(800, 600);   //create a drawing canvas
+}
+
+function draw() {
+  fill(100);  //check this syntax with alpha value
+  rect(0, 0, width, height);
+  ellipse(400, 300, 80, 80);  //draw an ellipse
+   console.log("Hello World");
+}
